0296475741fb80ccc6f9672d9b4adf21c3551ab7
Antoniio <antonio.kialaa@gmail.com>
2019-12-17 20:46:36 +0100

    Ajout du fichier pong en js

---
+++ pong/pongGlobal.js
@@ -0,0 +1,58 @@
+// BALLE
+
+var tailleBalle = 5;
+
+var ballePositionX = 576;
+
+var ballePositionY = 302;
+
+var couleurBalle = "yellow";
+
+var bx = -1;
+
+var by = -1;
+
+// RAQUETTE Droite
+
+var largeurRaquette = 75;
+
+var longueurRaquette = 10;
+
+var positionXRaquette = 1135;
+
+var positionYRaquette = 275;
+
+var couleurRaquette = "blue";
+
+var rx = 1;
+
+var deplacer = 0;
+
+// RAQUETTE Gauche
+
+var largeurRaquette2 = 75;
+
+var longueurRaquette2 = 10;
+
+var positionXRaquette2 = 8;
+
+var positionYRaquette2 = 275;
+
+var couleurRaquette2 = "blue";
+
+var rx2 = 1;
+
+var deplacer2 = 0;
+
+// TERRAIN
+
+var terrainLongueur = 1152;
+
+var terrainLargeur = 604;
+
+// JEU
+
+var scoreD = 0;
+var scoreG = 0;
+var play = 0;
+var stop = 0;
