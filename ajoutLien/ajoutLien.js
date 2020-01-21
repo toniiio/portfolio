11877299eac85c67d27ada865c10847e85330685
Antoniio <antonio.kialaa@gmail.com>
2020-01-21 11:35:57 +0100

    ajout de la page html

--- ajoutLien/ajoutLien.js
+++ ajoutLien/ajoutLien.js
@@ -1,4 +1,4 @@
-/* 
+﻿/* 
 Activité 1
 */
 
@@ -36,13 +36,13 @@ function creerElementLien(lien) {
     var urlElt = document.createElement("span");
     urlElt.appendChild(document.createTextNode(lien.url));
 
-    // Cette ligne contient le titre et l'URL du lien
+    
     var ligneTitreElt = document.createElement("h4");
     ligneTitreElt.style.margin = "0px";
     ligneTitreElt.appendChild(titreElt);
     ligneTitreElt.appendChild(urlElt);
 
-    // Cette ligne contient l'auteur
+    
     var ligneDetailsElt = document.createElement("span");
     ligneDetailsElt.appendChild(document.createTextNode("Ajouté par " + lien.auteur));
 
@@ -55,12 +55,12 @@ function creerElementLien(lien) {
 }
 
 var contenuElt = document.getElementById("contenu");
-// Parcours de la liste des liens et ajout d'un élément au DOM pour chaque lien
+
 listeLiens.forEach(function (lien) {
     var lienElt = creerElementLien(lien);
     contenuElt.appendChild(lienElt);
 });
-// Crée et renvoie un élément DOM de type input
+
 function creerElementInput(placeholder, taille) {
     var inputElt = document.createElement("input");
     inputElt.type = "text";
@@ -71,7 +71,7 @@ function creerElementInput(placeholder, taille) {
 }
 
 var ajouterLienElt = document.getElementById("ajoutLien");
-// Gère l'ajout d'un nouveau lien
+
 ajouterLienElt.addEventListener("click", function () {
     var auteurElt = creerElementInput("Entrez votre nom", 20);
     var titreElt = creerElementInput("Entrez le titre du lien", 40);
@@ -88,40 +88,34 @@ ajouterLienElt.addEventListener("click", function () {
     formAjoutElt.appendChild(ajoutElt);
 
     var p = document.querySelector("p");
-    // Remplace le bouton d'ajout par le formulaire d'ajout
     p.replaceChild(formAjoutElt, ajouterLienElt);
 
-    // Ajoute le nouveau lien
     formAjoutElt.addEventListener("submit", function (e) {
-        e.preventDefault(); // Annule la publication du formulaire
+        e.preventDefault(); 
         
         var url = urlElt.value;
-        // Si l'URL ne commence ni par "http://" ni par "https://"
         if ((url.indexOf("http://") !== 0) && (url.indexOf("https://") !== 0)) {
-            // On la préfixe par "http://"
+            
             url = "http://" + url;
         }
-
-        // Création de l'objet contenant les données du nouveau lien
+      
         var lien = {
             titre: titreElt.value,
             url: url,
             auteur: auteurElt.value
         };
 
-        var lienElt = creerElementLien(lien);
-        // Ajoute le nouveau lien en haut de la liste
+        var lienElt = creerElementLien(lien);       
         contenuElt.insertBefore(lienElt, contenuElt.firstChild);
 
-        // Remplace le formulaire d'ajout par le bouton d'ajout
         p.replaceChild(ajouterLienElt, formAjoutElt);
 
-        // Création du message d'information
+  
         var infoElt = document.createElement("div");
         infoElt.classList.add("info");
         infoElt.textContent = "Le lien \"" + lien.titre + "\" a bien été ajouté.";
         p.insertBefore(infoElt, ajouterLienElt);
-        // Suppresion du message après 2 secondes
+       
         setTimeout(function () {
             p.removeChild(infoElt);
         }, 2000);
